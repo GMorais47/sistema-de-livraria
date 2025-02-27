bb76ef52abdc59e3d70d9d609cb65699c405f3c3
Gustavo Morais Sousa <gustavomorais47.gm@gmail.com>
2025-02-26 22:57:45 -0300

    refactor: Refatoração do código

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
 import model.*;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -13,164 +9,71 @@ public class Main {
         boolean isLoop = true;
 
         while (isLoop) {
-            int escolha;
-            System.out.println("\n========== MENU ==========");
-            System.out.println("[ 1 ] - Cadastrar Autor;");
-            System.out.println("[ 2 ] - Listar Autores;");
-            System.out.println("[ 3 ] - Cadastrar Livro;");
-            System.out.println("[ 4 ] - Listar Livros Disponíveis;");
-            System.out.println("[ 5 ] - Cadastrar Cliente;");
-            System.out.println("[ 6 ] - Listar Cliente;");
-            System.out.println("[ 7 ] - Cadastrar Empréstimo;");
-            System.out.println("[ 8 ] - Listar Empréstimos Ativos;");
-            System.out.println("[ 9 ] - Devolver livro;");
-            System.out.println("\n[ 0 ] - Sair;");
-            System.out.print("\nEscolha uma das opções: ");
-            escolha = scanner.nextInt();
+            int escolha = -1;
 
-            scanner.nextLine();
+            printMenu();
 
-            switch (escolha){
+            if (scanner.hasNextInt()) {
+                escolha = scanner.nextInt();
+                scanner.nextLine();
+            } else {
+                System.out.println("[ ERROR ] - Entrada inválida! Insira um número.");
+                scanner.nextLine();
+                continue;
+            }
+
+            switch (escolha) {
                 case 0:
                     isLoop = false;
                     break;
                 case 1:
-                    System.out.println("\n========== CADASTRAR AUTOR ==========");
-                    System.out.print("\nQual o nome do autor: ");
-                    String nome = scanner.nextLine().toUpperCase();
-                    int quantidadeAutores = biblioteca.getAutores().size();
-                    Autor novoAutor = new Autor(++quantidadeAutores, nome);
-                    biblioteca.addAutor(novoAutor);
-                    System.out.println(String.format("O(a) %s foi cadastrado(a) com sucesso!", novoAutor.getNome()));
+                    biblioteca.cadastrarAutor(scanner);
                     break;
                 case 2:
-                    System.out.println("\n========== LISTA DE AUTORES ==========");
-                    if (biblioteca.getAutores().isEmpty()) {
-                        System.out.println("Nenhum autor cadastrado!");
-                    } else {
-                        for (Autor autor : biblioteca.getAutores()) {
-                            System.out.println(String.format("%d - %s", autor.getId(), autor.getNome()));
-                        }
-                    }
+                    biblioteca.listarAutores();
                     break;
                 case 3:
-                    System.out.println("\n========== CADASTRAR LIVRO ==========");
-                    System.out.print("\nQual o título do livro: ");
-                    String titulo = scanner.nextLine().toUpperCase();
-                    System.out.print("\nQual o código do autor: ");
-                    int autorID = scanner.nextInt();
-                    Autor autorSelecionado = biblioteca.getAutorById(autorID);
-                    if(autorSelecionado != null){
-                        int quantidadeLivros = biblioteca.getLivros().size();
-                        Livro novoLivro = new Livro(++quantidadeLivros,titulo,autorSelecionado);
-                        biblioteca.addLivro(novoLivro);
-                        System.out.println(String.format("O livro %s foi cadastrado(a) com sucesso!", novoLivro.getTitulo()));
-                    }else {
-                        System.out.println("Autor não encontrado!");
-                    }
+                    biblioteca.cadastrarLivro(scanner);
                     break;
                 case 4:
-                    System.out.println("\n========== LISTA DE LIVROS DISPONÍVEIS ==========");
-                    List<Livro> livrosDisponiveis = biblioteca.getLivrosDisponiveis();
-                    if (livrosDisponiveis.isEmpty()) {
-                        System.out.println("Nenhum livro disponível!");
-                    } else {
-                        for (Livro livro : livrosDisponiveis) {
-                            System.out.println(String.format("%d - %s", livro.getId(), livro.getTitulo()));
-                        }
-                    }
+                    biblioteca.listarLivros();
                     break;
                 case 5:
-                    System.out.println("\n========== CADASTRAR CLIENTE ==========");
-                    System.out.print("\nQual o nome do cliente: ");
-                    String nomeCliente = scanner.nextLine().toUpperCase();
-                    int quantidadeClientes = biblioteca.getClientes().size();
-                    Cliente novoCliente = new Cliente(++quantidadeClientes, nomeCliente);
-                    biblioteca.addCliente(novoCliente);
-                    System.out.println(String.format("O(a) cliente %s foi cadastrado(a) com sucesso!", novoCliente.getNome()));
+                    biblioteca.cadastrarCliente(scanner);
                     break;
                 case 6:
-                    System.out.println("\n========== LISTA DE CLIENTES ==========");
-                    List<Cliente> clientes = biblioteca.getClientes();
-                    if (clientes.isEmpty()) {
-                        System.out.println("Nenhum cliente cadastrado!");
-                    } else {
-                        for (Cliente cliente : clientes) {
-                            System.out.println(String.format("%d - %s", cliente.getId(), cliente.getNome()));
-                        }
-                    }
+                    biblioteca.listarClientes();
                     break;
                 case 7:
-                    System.out.println("\n========== CADASTRAR EMPRÉSTIMO ==========");
-                    System.out.print("\nQual o código do livro: ");
-                    int livroID = scanner.nextInt();
-                    System.out.print("\nQual o código do cliente: ");
-                    int clienteID = scanner.nextInt();
-                    scanner.nextLine();
-
-                    Livro livroSelecionado = biblioteca.getLivroById(livroID);
-                    Cliente clienteSelecionado = biblioteca.getClienteById(clienteID);
-
-                    if (livroSelecionado != null && clienteSelecionado != null) {
-                        if (livroSelecionado.isDisponivel()) {
-                            int quantidadeEmprestimos = biblioteca.getEmprestimos().size();
-                            Emprestimo novoEmprestimo = new Emprestimo(++quantidadeEmprestimos, livroSelecionado, clienteSelecionado);
-                            biblioteca.addEmprestimo(novoEmprestimo);
-                            livroSelecionado.setDisponivel(false);
-                            System.out.println("Empréstimo cadastrado com sucesso!");
-                        } else {
-                            System.out.println("Este livro já está emprestado!");
-                        }
-                    } else {
-                        System.out.println("Livro ou cliente não encontrado!");
-                    }
+                    biblioteca.cadastrarEmprestimo(scanner);
                     break;
-
                 case 8:
-                    System.out.println("\n========== LISTA DE EMPRÉSTIMOS ATIVOS ==========");
-                    List<Emprestimo> emprestimos = biblioteca.getEmprestimosAtivos();
-                    if (emprestimos.isEmpty()) {
-                        System.out.println("Nenhum empréstimo ativo cadastrado!");
-                    } else {
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
-
-                        for (Emprestimo emprestimo : emprestimos) {
-                            String dataFormatada = emprestimo.getDataCriacao().format(formatter);
-                            System.out.println(String.format("%d - %s - %s - %s",
-                                    emprestimo.getId(),
-                                    dataFormatada,
-                                    emprestimo.getLivro().getTitulo(),
-                                    emprestimo.getCliente().getNome()));
-                        }
-                    }
+                    biblioteca.listarEmprestimosAtivos();
                     break;
                 case 9:
-                    System.out.println("\n========== DEVOLVER LIVRO ==========");
-                    System.out.print("\nQual o código do empréstimo: ");
-                    int codigoEmprestimo = scanner.nextInt();
-                    scanner.nextLine();
-
-                    Emprestimo emprestimoSelecionado = biblioteca.getEmprestimoById(codigoEmprestimo);
-                    if(emprestimoSelecionado != null){
-                        if(emprestimoSelecionado.getDataDevolucao() == null){
-                            Livro livro = emprestimoSelecionado.getLivro();
-                            livro.setDisponivel(true);
-                            emprestimoSelecionado.setDataDevolucao(LocalDateTime.now());
-                            System.out.println(String.format("O empréstimo %d foi devolvido com sucesso!", emprestimoSelecionado.getId()));
-                        }else {
-                            System.out.println("Esse empréstimo já foi devolvido!");
-                        }
-                    }else {
-                        System.out.println("Empréstimo não encontrado!");
-                    }
+                    biblioteca.devolverEmprestimo(scanner);
                     break;
                 default:
                     System.out.println("[ ERROR ] - Opção inválida! Escolha uma das opções fornecidas!\n");
-                    break;
             }
         }
 
         scanner.close();
         System.out.println("Finalizando a aplicação!");
+    }
+
+    private static void printMenu(){
+        System.out.println("\n========== MENU ==========");
+        System.out.println("[ 1 ] - Cadastrar Autor;");
+        System.out.println("[ 2 ] - Listar Autores;");
+        System.out.println("[ 3 ] - Cadastrar Livro;");
+        System.out.println("[ 4 ] - Listar Livros Disponíveis;");
+        System.out.println("[ 5 ] - Cadastrar Cliente;");
+        System.out.println("[ 6 ] - Listar Cliente;");
+        System.out.println("[ 7 ] - Cadastrar Empréstimo;");
+        System.out.println("[ 8 ] - Listar Empréstimos Ativos;");
+        System.out.println("[ 9 ] - Devolver livro;");
+        System.out.println("\n[ 0 ] - Sair;");
+        System.out.print("\nEscolha uma das opções: ");
     }
 }
--- src/model/Biblioteca.java
+++ src/model/Biblioteca.java
@@ -1,97 +1,181 @@
 package model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Biblioteca {
 
-    List<Autor> autores = new ArrayList<>();
-    List<Livro> livros = new ArrayList<>();
-    List<Cliente> clientes = new ArrayList<>();
-    List<Emprestimo> emprestimos = new ArrayList<>();
+    private List<Autor> autores = new ArrayList<>();
+    private List<Livro> livros = new ArrayList<>();
+    private List<Cliente> clientes = new ArrayList<>();
+    private List<Emprestimo> emprestimos = new ArrayList<>();
 
-    public void addAutor(Autor autor){
-        autores.add(autor);
+    private int autorIdCounter = 0;
+    private int livroIdCounter = 0;
+    private int clienteIdCounter = 0;
+    private int emprestimoIdCounter = 0;
+
+    // Métodos de busca
+    private Autor getAutorById(int id) {
+        return autores.stream().filter(a -> a.getId() == id).findFirst().orElse(null);
     }
 
-    public List<Autor>getAutores(){
-        return autores;
+    private Livro getLivroById(int id) {
+        return livros.stream().filter(l -> l.getId() == id).findFirst().orElse(null);
     }
 
-    public Autor getAutorById(int id){
-        for (Autor autor : autores){
-            if(autor.getId() == id)
-             return autor;
+    private Cliente getClienteById(int id) {
+        return clientes.stream().filter(c -> c.getId() == id).findFirst().orElse(null);
+    }
+
+    private Emprestimo getEmprestimoById(int id) {
+        return emprestimos.stream().filter(e -> e.getId() == id).findFirst().orElse(null);
+    }
+
+    private List<Emprestimo> getEmprestimosAtivos() {
+        return emprestimos.stream().filter(e -> e.getDataDevolucao() == null).toList();
+    }
+
+    // Métodos de adição
+    private void addAutor(Autor autor) { autores.add(autor); }
+    private void addLivro(Livro livro) { livros.add(livro); }
+    private void addCliente(Cliente cliente) { clientes.add(cliente); }
+    private void addEmprestimo(Emprestimo emprestimo) { emprestimos.add(emprestimo); }
+
+    // Métodos principais
+    public void cadastrarAutor(Scanner scanner) {
+        System.out.println("\n========== CADASTRAR AUTOR ==========");
+        System.out.print("\nQual o nome do autor: ");
+        String nome = scanner.nextLine().toUpperCase();
+
+        Autor novoAutor = new Autor(++autorIdCounter, nome);
+        addAutor(novoAutor);
+
+        System.out.println(String.format("O(a) %s foi cadastrado(a) com sucesso!", novoAutor.getNome()));
+    }
+
+    public void listarAutores() {
+        System.out.println("\n========== LISTA DE AUTORES ==========");
+        if (autores.isEmpty()) {
+            System.out.println("Nenhum autor cadastrado!");
+        } else {
+            autores.forEach(a -> System.out.println(a.getId() + " - " + a.getNome()));
         }
-        return null;
     }
 
-    public List<Livro> getLivros(){
-        return livros;
-    }
+    public void cadastrarLivro(Scanner scanner) {
+        System.out.println("\n========== CADASTRAR LIVRO ==========");
+        System.out.print("\nQual o título do livro: ");
+        String titulo = scanner.nextLine().toUpperCase();
 
-    public Livro getLivroById(int id){
-        for (Livro livro : livros){
-            if(livro.getId() == id)
-                return livro;
+        System.out.print("\nQual o código do autor: ");
+        int autorID = scanner.nextInt();
+        scanner.nextLine();
+
+        Autor autorSelecionado = getAutorById(autorID);
+
+        if (autorSelecionado != null) {
+            Livro novoLivro = new Livro(++livroIdCounter, titulo, autorSelecionado);
+            addLivro(novoLivro);
+            System.out.println("Livro cadastrado com sucesso!");
+        } else {
+            System.out.println("Autor não encontrado!");
         }
-        return null;
     }
 
-    public List<Livro> getLivrosDisponiveis(){
-        List<Livro> disponiveis = new ArrayList<>();
-
-        for (Livro livro : livros){
-            if(livro.isDisponivel())
-                disponiveis.add(livro);
+    public void listarLivros() {
+        System.out.println("\n========== LISTA DE LIVROS ==========");
+        if (livros.isEmpty()) {
+            System.out.println("Nenhum livro cadastrado!");
+        } else {
+            livros.forEach(l -> {
+                String status = l.isDisponivel() ? "Disponível" : "Emprestado";
+                System.out.println(l.getId() + " - " + l.getTitulo() + " (" + status + ")");
+            });
         }
-
-        return disponiveis;
     }
 
-    public void addLivro(Livro livro){
-        livros.add(livro);
+    public void cadastrarCliente(Scanner scanner) {
+        System.out.println("\n========== CADASTRAR CLIENTE ==========");
+        System.out.print("\nQual o nome do cliente: ");
+        String nomeCliente = scanner.nextLine().toUpperCase();
+
+        Cliente novoCliente = new Cliente(++clienteIdCounter, nomeCliente);
+        addCliente(novoCliente);
+
+        System.out.println("Cliente cadastrado com sucesso!");
     }
 
-    public void addCliente(Cliente cliente){
-        clientes.add(cliente);
-    }
-
-    public List<Cliente> getClientes(){
-        return clientes;
-    }
-
-    public Cliente getClienteById(int id){
-        for (Cliente cliente : clientes){
-            if(cliente.getId() == id)
-                return cliente;
+    public void listarClientes() {
+        System.out.println("\n========== LISTA DE CLIENTES ==========");
+        if (clientes.isEmpty()) {
+            System.out.println("Nenhum cliente cadastrado!");
+        } else {
+            clientes.forEach(c -> System.out.println(c.getId() + " - " + c.getNome()));
         }
-        return null;
     }
 
-    public List<Emprestimo> getEmprestimos(){
-        return emprestimos;
-    }
+    public void cadastrarEmprestimo(Scanner scanner) {
+        System.out.println("\n========== CADASTRAR EMPRÉSTIMO ==========");
+        System.out.print("\nQual o código do livro: ");
+        int livroID = scanner.nextInt();
+        System.out.print("\nQual o código do cliente: ");
+        int clienteID = scanner.nextInt();
+        scanner.nextLine();
 
-    public List<Emprestimo> getEmprestimosAtivos(){
-        List<Emprestimo> emprestimosAtivos = new ArrayList<>();
-        for (Emprestimo emprestimo : emprestimos){
-            if( emprestimo.getDataDevolucao() == null)
-                emprestimosAtivos.add(emprestimo);
+        Livro livroSelecionado = getLivroById(livroID);
+        Cliente clienteSelecionado = getClienteById(clienteID);
+
+        if (livroSelecionado != null && clienteSelecionado != null) {
+            if (livroSelecionado.isDisponivel()) {
+                Emprestimo novoEmprestimo = new Emprestimo(++emprestimoIdCounter, livroSelecionado, clienteSelecionado);
+                addEmprestimo(novoEmprestimo);
+                livroSelecionado.setDisponivel(false);
+                System.out.println("Empréstimo cadastrado com sucesso!");
+            } else {
+                System.out.println("Este livro já está emprestado!");
+            }
+        } else {
+            System.out.println("Livro ou cliente não encontrado!");
         }
-        return emprestimosAtivos;
     }
 
-    public Emprestimo getEmprestimoById(int id){
-        for (Emprestimo emprestimo : emprestimos){
-            if(emprestimo.getId() == id)
-                return emprestimo;
+    public void devolverEmprestimo(Scanner scanner) {
+        System.out.println("\n========== DEVOLVER EMPRÉSTIMO ==========");
+        System.out.print("\nQual o código do empréstimo: ");
+        int codigoEmprestimo = scanner.nextInt();
+        scanner.nextLine();
+
+        Emprestimo emprestimoSelecionado = getEmprestimoById(codigoEmprestimo);
+
+        if (emprestimoSelecionado != null && emprestimoSelecionado.getDataDevolucao() == null) {
+            emprestimoSelecionado.getLivro().setDisponivel(true);
+            emprestimoSelecionado.setDataDevolucao(LocalDateTime.now());
+            System.out.println("Empréstimo devolvido com sucesso!");
+        } else {
+            System.out.println("Empréstimo não encontrado ou já devolvido!");
         }
-        return null;
     }
 
-    public void addEmprestimo(Emprestimo emprestimo){
-        emprestimos.add(emprestimo);
-    }
+    public void listarEmprestimosAtivos() {
+        System.out.println("\n========== LISTA DE EMPRESTIMOS ATIVOS ==========");
+        List<Emprestimo> emprestimosAtivos = this.getEmprestimosAtivos();
+        if (emprestimosAtivos.isEmpty()) {
+            System.out.println("Nenhum emprestimo cadastrado!");
+        } else {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
 
+            emprestimosAtivos.forEach(emprestimo -> {
+                String dataFormatada = emprestimo.getDataCriacao().format(formatter);
+                System.out.println(String.format("%d - %s - %s - %s",
+                        emprestimo.getId(),
+                        dataFormatada,
+                        emprestimo.getLivro().getTitulo(),
+                        emprestimo.getCliente().getNome()));
+            });
+        }
+    }
 }
